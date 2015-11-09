75f5fc676dbe5ea4033615459ede290831cd9dfb
VIPArcher <VIPArcher@sina.com>
2015-11-09 14:31:49 +0800

    Update main.js

--- small-laly/js/main.js
+++ small-laly/js/main.js
@@ -64,7 +64,7 @@ function get_5result(result_array,i){
     var img_url = result_array[1].replace(/http:\/\/image.rakuten.co.jp\/small-laly\/(.*?).jpg/i, "$1");
     var iscontaintest = img_url.indexOf(".jpg")== -1 ? false : true;
     var result = '<li><div class="ureitem">'
-    if (i <= 2) {position = 'right'} else{position = 'left'};
+    if (i <= 3) {position = 'right'} else{position = 'left'};
     if (document.getElementById('tax_' + (i + 1)).checked) {tax_text = '<span >(税込) 送料別</span>'};
     result += '<a href="' + result_array[0] + '" target="_top" bigimage="' + result_array[1] + '" class="viparcher_image_zoom_list" position="' + position + '" >'
     if (iscontaintest) {
@@ -96,4 +96,36 @@ function show_5result(){
         });
     });
     $("#result").val('<div id="ranking"><div style="font-size:11px;margin:5px 0;font-family:arial">'+ get_time() + '更新</div><div id="ookan"><li><div class="ookanitem"><img src="images/rank_1.jpg" border="0"></div></li><li><div class="ookanitem"><img src="images/rank_2.jpg" border="0"></div></li><li><div class="ookanitem"><img src="images/rank_3.jpg" border="0"></div></li><li><div class="ookanitem"><img src="images/rank_4.jpg" border="0"></div></li><li><div class="ookanitem"><img src="images/rank_5.jpg" border="0"></div></li></div><div id="rankitem">' + string + '</div></div>')
+}
+
+// 获取8套结果列表
+function get_daynewresult(result_array,i){
+    var tax_text = ''
+    var result = '<li>'
+    var img_url = result_array[1].replace(/http:\/\/image.rakuten.co.jp\/small-laly\/(.*?).jpg/i, "$1");
+    var iscontaintest = img_url.indexOf(".jpg")== -1 ? false : true;
+    if (document.getElementById('tax_' + (i + 1)).checked) {tax_text = '<span >(税込) 送料別</span>'};
+    result += '<a href="' + result_array[0] + '" target="_blank">'
+    if (iscontaintest) {
+        result += '<img src="' + img_url + '"></a><br>'
+    } else{
+        result += '<img src="http://thumbnail.image.rakuten.co.jp/@0_mall/small-laly/' + img_url + '.jpg?_ex=250x250&s=2&r=1"></a><br>'
+    };
+    result += '<p class="name">' + result_array[2] + '</p>'
+    result += '<p class="price">' + result_array[3] + tax_text + '</p>'
+    result += '</li>'
+    return result
+}
+// 显示8套最终结果
+function show_daynewresult(){
+    var result_array = [[0],[0],[0],[0],[0],[0],[0],[0]]
+    var string = ""
+    $.each(result_array, function(i) {
+        result_array[i][0] = check_null($('#goods-link_' + (i + 1)).val(),'#goods-link_' + (i + 1))
+        result_array[i][1] = check_null($('#image-url_' + (i + 1)).val(),'#image-url_' + (i + 1))
+        result_array[i][2] = check_null($('#goods-note_' + (i + 1)).val(),'#goods-note_' + (i + 1))
+        result_array[i][3] = check_null($('#goods-price_' + (i + 1)).val(),'#goods-price_' + (i + 1))
+        string += get_daynewresult(result_array[i], i)
+    });
+    $("#result").val('<div class="top"><ul>' + string + "</ul></div>")
 }
